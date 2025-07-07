c4337a919b79e1bd71767002f63790b0d12c3509
Farhad Mani <frhdmani@gmail.com>
2025-07-08 00:50:35 +0200

    feat: Add tests

---
+++ tests/test_downloader.py
@@ -0,0 +1,15 @@
+"""
+Tests for downloader module
+"""
+
+import pytest
+from gmail_downloader.downloader import *
+
+class TestDownloader:
+    """Test cases for downloader"""
+    
+    def test_placeholder(self):
+        """Placeholder test - TODO: Implement real tests"""
+        assert True
+        
+    # TODO: Add more tests
---
+++ tests/test_gmail_client.py
@@ -0,0 +1,15 @@
+"""
+Tests for gmail_client module
+"""
+
+import pytest
+from gmail_downloader.gmail_client import *
+
+class TestGmailClient:
+    """Test cases for gmail_client"""
+    
+    def test_placeholder(self):
+        """Placeholder test - TODO: Implement real tests"""
+        assert True
+        
+    # TODO: Add more tests
